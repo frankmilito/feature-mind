5b25917390be55060d613ae814d6cf5b2e9fe5b3
frankmilito <frankobidike20@gmail.com>
2024-10-24 15:42:51 +0100

    Added responsiveness

--- src/components/Movies.tsx
+++ src/components/Movies.tsx
@@ -10,12 +10,12 @@ export const Movies: React.FC<Movie> = ({
   Type,
 }) => {
   return (
-    <Link to={`/movie/${imdbID}`} key={imdbID} className="mx-auto">
+    <Link to={`/movie/${imdbID}`} key={imdbID}>
       <div className="p-4 bg-gray-200 rounded">
         <img
           src={Poster}
           alt={Title}
-          className="object-cover w-full md:w-72 h-60"
+          className="object-cover w-full h-60"
           loading="lazy"
         />
         <h3 className="mt-2 text-sm font-semibold">{Title}</h3>
